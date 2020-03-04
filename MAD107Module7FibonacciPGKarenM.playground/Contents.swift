55a567aa9043d333dc9c6437376a8b1cb00ac3df
km <35552602+catnap7777@users.noreply.github.com>
2020-03-04 16:46:05 -0600

    initial commit

--- MAD107Module7FibonacciPGKarenM.playground/Contents.swift
+++ MAD107Module7FibonacciPGKarenM.playground/Contents.swift
@@ -5,20 +5,18 @@ var str = "Hello, playground"
 // returns an array containing the first n Fibonacci numbers
 func fibonacci(n: Int) -> [Int] {
 
-    //assert(n > 1)
-
     var array = [0, 1]
     
     guard n > 1 else {
         return array
     }
 
-    while array.count < n {
+    while array.count <= n {
         array.append(array[array.count - 1] + array[array.count - 2])
     }
     return array
 }
 
-var kamArray = fibonacci(n: 15) // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+var kamArray = fibonacci(n:15) // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
 print(kamArray)
 
